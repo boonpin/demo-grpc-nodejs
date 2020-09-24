0f71f2542cb10363e69f1123eaf30f573aa763ec
Kelvin <wongbp@gmail.com>
2020-09-24 11:58:07 +0800

    add command proto (allow server to run a command to get data from client)

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ const grpc = require('grpc');
 const protoPackage = require('./package');
 const os = require('os');
 const jwt = require('jsonwebtoken');
+const moment = require('moment');
 
 function heartbeat() {
     return {
@@ -17,24 +18,25 @@ function heartbeat() {
 }
 
 function main() {
-    const client = new protoPackage.heartbeat.HeartbeatService('localhost:50051', grpc.credentials.createInsecure());
     const meta = new grpc.Metadata();
     meta.add('Authorization', jwt.sign({
         hostname: os.hostname()
     }, 'my-secret-1234', {expiresIn: `5m`}));
 
+    runHeartbeat(meta);
+    runCommand(meta);
+}
 
+function runHeartbeat(meta) {
+    const client = new protoPackage.heartbeat.HeartbeatService('localhost:80', grpc.credentials.createInsecure());
     const connection = client.Establish(meta);
-
     const interval = setInterval(() => {
-        connection.write(heartbeat(), meta)
-    }, 2500)
-
+        connection.write(heartbeat())
+    }, 60000)
     const stop = (msg) => () => {
         console.log(msg);
         clearInterval(interval);
     };
-
     connection.on('data', function (d) {
         console.log(d);
     });
@@ -42,5 +44,26 @@ function main() {
     connection.on('cancelled', stop('cancelled'));
 }
 
+function runCommand(meta) {
+    const client = new protoPackage.command.CommandService('localhost:80', grpc.credentials.createInsecure());
+    const connection = client.Establish(meta);
+    connection.on('data', function (request) {
+        const {uid, action, data} = request;
+
+        const dObject = JSON.parse(JSON.parse(data));
+
+        console.log(`action = ${action}`);
+
+        switch (action) {
+            case 'SHOW_TIME':
+                connection.write({uid, data: moment().format(dObject.format)})
+                break;
+            default:
+                connection.write({uid, error: `unhandled action ${action}`});
+        }
+    });
+    connection.on('end', () => console.log('end'));
+    connection.on('cancelled', () => console.log('cancelled'));
+}
 
 main();
--- src/package.js
+++ src/package.js
@@ -2,7 +2,10 @@ const grpc = require('grpc');
 const path = require('path');
 const protoLoader = require('@grpc/proto-loader');
 const packageDefinition = protoLoader.loadSync(
-    path.join(__dirname, 'proto', 'heartbeat.proto'), {
+    [
+        path.join(__dirname, 'proto', 'command.proto'),
+        path.join(__dirname, 'proto', 'heartbeat.proto'),
+    ], {
         keepCase: true,
         longs: String,
         enums: String,
